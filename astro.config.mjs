cb26312193a65b8c483368fcc2f1e73da13ebaa4
Juventino-E <juventino@kavaup.io>
2026-02-16 07:48:43 -0600

    Disable CSRF check for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,15 @@
-import vercel from '@astrojs/vercel/serverless'; 
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
 
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-
-    vite: {
-    server: {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      }
-    }
+  
+  security: {
+    checkOrigin: false
   },
-
+  
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en'],
@@ -22,13 +17,13 @@ export default defineConfig({
       prefixDefaultLocale: false
     }
   },
-
+  
   site: 'https://portfolio-juve.vercel.app',
   trailingSlash: 'never',
-
+  
   build: {
     inlineStylesheets: 'auto'
   },
-
+  
   integrations: [react()]
 });
